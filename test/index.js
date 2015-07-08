c2451ad67be2c329d29025aa66be0268be1a6971
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-07-07 23:59:18 -0400

    Remove removal test.

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var path = require('path');
 var TemporaryRepository = require('./_helpers').Repository;
 
 describe('stylish-commit', function () {
-  it('automatically installs and removes a git pre-commit hook on npm install/remove', function () {
+  it('automatically installs a git pre-commit hook on npm install', function () {
     this.timeout(10000);
     var temporaryRepository = new TemporaryRepository();
     temporaryRepository.createFile('package.json', '{ "name": "test", "version": "0.0.1" }');
@@ -14,8 +14,5 @@ describe('stylish-commit', function () {
     temporaryRepository.runAtRoot('npm install --save "' + moduleRoot + '"');
     var precommitFileContents = temporaryRepository.getFileContents('.git/hooks/pre-commit');
     assert.include(precommitFileContents, 'stylish-commit precommit hook');
-
-    temporaryRepository.runAtRoot('npm --verbose uninstall --save stylish-commit');
-    assert.notOk(temporaryRepository.containsFile('.git/hooks/pre-commit'));
   });
 });
